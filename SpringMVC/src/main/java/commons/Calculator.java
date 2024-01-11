4a9b0df92e447cf380e99ec7c60ceace813fea70
hyemi-bang <pbhm1124@gmail.com>
2024-01-11 16:25:39 +0900

    0111

--- SpringMVC/src/main/java/commons/Calculator.java
+++ SpringMVC/src/main/java/commons/Calculator.java
@@ -1,9 +1,9 @@
 package commons;
 
 public class Calculator {
-	
-	public int diff(int a, int b) {
-		return Math.abs(a - b);
-	}
-
+   
+   public int diff(int a, int b) {
+      return Math.abs(a- b);
+   }
+   
 }
